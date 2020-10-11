f8d0b061cd434801dd154c86e4b4aea1512f8ea2
tmadau <git@gitlab.uber5.com:tmadau>
2020-10-11 23:05:26 +0200

    front end structure for landing, login and registration with links and styling

--- client-front-end/src/App.js
+++ client-front-end/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import Register from './component/Register';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import './App.css';
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <p>I am a component, yay!!!</p>
-        <Register />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={ Register } />
+          <Route exact path="/login" component={ Login } />
+        </div>
+      </Router>
     );
   }
 }
